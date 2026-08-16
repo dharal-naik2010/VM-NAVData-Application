3b8ee17739509d6589a0460d4eeca31ce7cca4f9
Dharal Naik <naik.dharal20@gmail.com>
2026-08-16 09:44:03 +0530

    Data ingestion with .CSV or .xlsx implemented.

---
+++ data_ingestion.py
@@ -0,0 +1,19 @@
+import pandas as pd 
+
+def load_data(filename: str):
+    if not filename.endswith(".csv", ".xlsx"):
+        raise ValueError("Filename must be an Excel or a CSV file")
+    if filename .endswith(".csv"):
+        df = pd.read_csv(filename)
+    elif filename.endswith(".xlsx"):
+        df = pd.read_excel(filename)
+
+    required_columns = ["Fund Name", "Date", "NAV"]
+
+    if list(df.columns) != required_columns:
+        raise ValueError(f"Expected columns: {required_columns}, Found: {list(df.columns)}")
+        
+    df["Date"] = pd.to_datetime(df["Date"])
+    df["NAV"] = pd.to_numeric(df["NAV"])
+
+    return df 
